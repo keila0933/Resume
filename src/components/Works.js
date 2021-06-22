320a200d941bebea7460dab1e2e403bd2c914487
keila0933 <keila0933@gmail.com>
2021-06-22 21:27:01 +0800

    add work-content

--- src/components/Works.js
+++ src/components/Works.js
@@ -9,7 +9,7 @@ import SwiperCore, {
 } from 'swiper/core'
 
 // install Swiper modules
-SwiperCore.use([EffectCoverflow, Pagination, Navigation])
+SwiperCore.use([EffectCoverflow, Pagination])
 
 const works = [
   {
@@ -41,40 +41,35 @@ export default function Works() {
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
-          depth: 100,
+          depth: 200,
           modifier: 1,
           slideShadows: false,
         }}
         pagination={true}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <a href="https://keila0933.github.io/Resume/">
-            <img src="https://keila0933.github.io/Resume/images/work_resume.JPG" />
-            <div className="workInfo">
-              <div className="workTitle">Title1</div>
-              <div className="workTool">Tool</div>
-            </div>
-          </a>
+        <SwiperSlide tag="a" href="https://keila0933.github.io/Resume/">
+          <img src="https://keila0933.github.io/Resume/images/work_resume.JPG" />
+          <div className="workInfo">
+            <p className="workTitle">Portfolio Website</p>
+            <p className="workTool">Webpack | React.js | Swiper | SCSS</p>
+          </div>
         </SwiperSlide>
-
-        <SwiperSlide>
-          <a href="https://fcy-shoes-store.herokuapp.com/">
-            <img src="https://keila0933.github.io/Resume/images/work_store.JPG" />
-            <div className="workInfo">
-              <div className="workTitle">Title2</div>
-              <div className="workTool">Tool</div>
-            </div>
-          </a>
+        <SwiperSlide tag="a" href="https://fcy-shoes-store.herokuapp.com/">
+          <img src="https://keila0933.github.io/Resume/images/work_store.JPG" />
+          <div className="workInfo">
+            <p className="workTitle">Shoes Store</p>
+            <p className="workTool">
+              React.js | Hook | Heroku <br /> JSON Sever | JWT
+            </p>
+          </div>
         </SwiperSlide>
-        <SwiperSlide>
-          <a href="https://keila0933.github.io/paint-board/">
-            <img src="https://keila0933.github.io/Resume/images/work_paint.JPG" />
-            <div className="workInfo">
-              <div className="workTitle">Title3</div>
-              <div className="workTool">Tool</div>
-            </div>
-          </a>
+        <SwiperSlide tag="a" href="https://keila0933.github.io/paint-board/">
+          <img src="https://keila0933.github.io/Resume/images/work_paint.JPG" />
+          <div className="workInfo">
+            <p className="workTitle">Paint Board</p>
+            <p className="workTool">JS | Canvas | CSS</p>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
